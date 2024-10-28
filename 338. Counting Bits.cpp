322a95e43c4acbed0803e89dd158031426c76cc7
Peter Refaat <141999832+Peter-Refaat@users.noreply.github.com>
2024-10-28 18:02:20 +0300

    Create 338. Counting Bits.cpp

---
+++ 338. Counting Bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>ans;
+        for(int i = 0 ; i <= n ; ++i)
+        {
+            bitset<32>b(i);
+            ans.push_back(b.count());
+        }
+        return ans;
+    }
+};
