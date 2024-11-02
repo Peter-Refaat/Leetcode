535ae06b22dbb26d54385ca999addacf67a24b3d
Peter Refaat <141999832+Peter-Refaat@users.noreply.github.com>
2024-11-02 19:49:34 +0200

    Update 182. Duplicate Emails.sql

--- 182. Duplicate Emails.sql
+++ 182. Duplicate Emails.sql
@@ -1,3 +1,10 @@
 SELECT DISTINCT i.email AS Email
 FROM person i , person e
 WHERE i.id = e.id AND i.email IN (SELECT i.email FROM person i , person e WHERE i.id != e.id AND i.email = e.email)
+
+--OR--
+
+SELECT email AS Email
+FROM person
+GROUP BY email
+HAVING COUNT(email) > 1;
