ebd98a73f1dee9403239dd3e1e5c4b764696bfb7
Peter Refaat <141999832+Peter-Refaat@users.noreply.github.com>
2025-01-26 13:37:47 +0200

    Create  Code Testcase Testcase Test Result 219. Contains Duplicate II.cpp

---
+++ Code Testcase Testcase Test Result 219. Contains Duplicate II.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,vector<int>>mp;
+        for(int i = 0 ; i < nums.size() ; ++i)
+            mp[nums[i]].push_back(i);
+        for(auto& it : mp)
+        {
+            for(int i = 1 ; i < it.second.size() ; ++i)
+            {
+                if(it.second[i] - it.second[i-1] <= k)
+                    return true;
+            }
+        }
+        return false;
+    }
+};
