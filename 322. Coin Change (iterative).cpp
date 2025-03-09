82481b63f0e9e37b690fe3af8948f72fc5dd76df
Peter Refaat <141999832+Peter-Refaat@users.noreply.github.com>
2025-03-09 11:11:27 +0200

    Create 322. Coin Change (iterative).cpp

---
+++ 322. Coin Change (iterative).cpp
@@ -0,0 +1,18 @@
+#define ll long long
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<ll> dp(amount+1,1e9);
+        dp[amount] = 0;
+        for(auto& c : coins)
+        {
+            for(int j = amount ; j >= 0 ; --j)
+            {
+                if(j + c * 1LL <= amount)
+                    dp[j] = min(dp[j],dp[j + c] + 1);
+            }
+        }
+        return (dp[0] >= 1e9 ? -1 : dp[0]);
+    }
+};
