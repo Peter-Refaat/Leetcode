b32d3ca250ba403bc696a79fb503c6d7e3c2c139
Peter Refaat <141999832+Peter-Refaat@users.noreply.github.com>
2025-03-10 14:05:15 +0200

    Update 69. Sqrt(x).cpp

--- 69. Sqrt(x).cpp
+++ 69. Sqrt(x).cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        int l = 0 , r = x , sq = 0;
+        while(l <= r)
+        {
+            long long m = l+r>>1;
+            if(m*m <= x)
+                sq = m, l = m+1;
+            else r = m-1;
+        }
+        return sq;
     }
 };
