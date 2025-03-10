1c3c90776ca16193d68423c4fbc7c4953e662b61
Peter Refaat <141999832+Peter-Refaat@users.noreply.github.com>
2025-03-10 14:32:15 +0200

    Update 633. Sum of Square Numbers.cpp

--- 633. Sum of Square Numbers.cpp
+++ 633. Sum of Square Numbers.cpp
@@ -1,3 +1,4 @@
+// O(sqrt(N)*log(N)
 class Solution {
 public:
     bool judgeSquareSum(int c) {
@@ -16,5 +17,23 @@ public:
                 return true;
         }
         return false;
+    }
+};
+
+//O(sqrt(N))
+//using fermat's theorem: Any positive number n is expressible as a sum of two squares if and only if 
+//the prime factorization of n, every prime of the form (4k+3) (prime%4==3) occurs an even number of times.
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        for(int i = 2 ; i*i <= c ; ++i)
+        {
+            int cnt = 0;
+            while(c%i==0)
+                cnt++ , c/=i;
+            if(i%4==3 and cnt&1)
+                return false;
+        }
+        return c%4!=3;
     }
 };
