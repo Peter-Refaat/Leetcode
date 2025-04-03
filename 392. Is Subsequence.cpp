913480cfb7640b5a1147a19af0fe89b8c007ef88
Peter Refaat <141999832+Peter-Refaat@users.noreply.github.com>
2025-04-03 21:07:39 +0200

    Create 392. Is Subsequence.cpp

---
+++ 392. Is Subsequence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int p1 = 0 , p2 = 0;
+        while(p1 < s.size() and p2 < t.size())
+        {
+            if(s[p1] == t[p2])
+                p1++ , p2++;
+            else p2++;
+        }
+        return p1 == s.size();
+    }
+};
